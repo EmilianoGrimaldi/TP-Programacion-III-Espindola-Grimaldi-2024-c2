c66302374adebeec3b6e051620419001f98215fc
Emiliano <grimaldi385@gmail.com>
2024-09-27 22:50:23 -0300

    correcciones y mas logos para elegir

--- files/js/landing.js
+++ files/js/landing.js
@@ -2,10 +2,10 @@
 
 function alertarNombre() {
   let nombre = document.getElementById("inputNombre").value;
-  alert(`Bienvenido ${nombre}`);
-  //direcciona al index deespues de aceptar el alert.
-  //- Hacer una validacion al input -
-  location.href = "index.html";
+  if (nombre != "") {
+    //- Hacer una validacion al input -
+    location.href = "index.html";
+  }
 }
 
 //evento click asociado al boton aceptar del form
