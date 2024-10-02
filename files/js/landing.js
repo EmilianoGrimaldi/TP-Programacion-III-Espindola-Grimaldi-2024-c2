d4f55e5ecc09254311e1d4f7145342129929b047
lucasespd <lucasesp.d@gmail.com>
2024-10-02 12:18:20 -0300

    funcionalidad para cambiar el tema

--- files/js/landing.js
+++ files/js/landing.js
@@ -6,6 +6,28 @@ function alertarNombre() {
   }
 }
 
+//funcion para cambiar el icono y el tema.
+function cambiarTema() {
+  if (html.getAttribute("data-bs-theme") == "light") {
+    iconoEnBoton.classList.remove("bi-brightness-high-fill");
+    iconoEnBoton.classList.add("bi-moon-stars-fill");
+    html.setAttribute("data-bs-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    iconoEnBoton.classList.add("bi-brightness-high-fill");
+    iconoEnBoton.classList.remove("bi-moon-stars-fill");
+    html.setAttribute("data-bs-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+//evento click enviado al icono
+let btnToggler = document.getElementById("btnToggler");
+//agarro el icono adentro del boton para que al hacer click lo cambie.
+let iconoEnBoton = btnToggler.querySelector("i");
+let html = document.getElementsByTagName("html")[0];
+btnToggler.addEventListener("click", cambiarTema);
+
 //evento click asociado al boton aceptar del form
 let btnAceptar = document.getElementById("btnAceptar");
 btnAceptar.addEventListener("click", alertarNombre);
