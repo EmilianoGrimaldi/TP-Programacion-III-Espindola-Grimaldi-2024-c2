2edfb36b4b70bc8629ecb474387ab64f908f2eca
lucasespd <lucasesp.d@gmail.com>
2024-10-19 19:40:43 -0300

    estilos y responsive a la landing

--- files/js/landing.js
+++ files/js/landing.js
@@ -13,11 +13,17 @@ function cambiarIconoSegunTema() {
     iconoToggler.classList.remove("bi-brightness-high-fill");
     iconoToggler.classList.add("bi-moon-stars-fill");
     html.setAttribute("data-bs-theme", "dark");
+
+    body.classList.add("dark-mode");
+
     localStorage.setItem("theme", "dark");
   } else {
     iconoToggler.classList.add("bi-brightness-high-fill");
     iconoToggler.classList.remove("bi-moon-stars-fill");
     html.setAttribute("data-bs-theme", "light");
+
+    body.classList.remove("dark-mode");
+
     localStorage.setItem("theme", "light");
   }
 }
@@ -30,11 +36,13 @@ function temaLocalStorage() {
       iconoToggler.classList.remove("bi-brightness-high-fill");
       iconoToggler.classList.add("bi-moon-stars-fill");
       html.setAttribute("data-bs-theme", "dark");
+      body.classList.add("dark-mode");
       break;
     case (null, "light"):
       iconoToggler.classList.add("bi-brightness-high-fill");
       iconoToggler.classList.remove("bi-moon-stars-fill");
       html.setAttribute("data-bs-theme", "light");
+      body.classList.remove("dark-mode");
       break;
   }
 }
@@ -42,6 +50,7 @@ function temaLocalStorage() {
 window.onload = temaLocalStorage;
 
 //evento click enviado al icono.
+let body = document.querySelector("body");
 let html = document.getElementsByTagName("html")[0];
 let iconoToggler = document.getElementById("iconoToggler");
 iconoToggler.addEventListener("click", cambiarIconoSegunTema);
