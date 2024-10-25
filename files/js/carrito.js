e0a34f066c6c7fd071b30eee8db484131c975599
lucasespd <lucasesp.d@gmail.com>
2024-10-25 17:01:41 -0300

    agregado carrito y algunos detalles

---
+++ files/js/carrito.js
@@ -0,0 +1,48 @@
+//funcion para cambiar el icono según el tema y lo guarda en localStorage.
+function cambiarIconoSegunTema() {
+  if (html.getAttribute("data-bs-theme") == "light") {
+    iconoToggler.classList.remove("bi-brightness-high-fill");
+    iconoToggler.classList.add("bi-moon-stars-fill");
+    html.setAttribute("data-bs-theme", "dark");
+
+    body.classList.add("dark-mode");
+
+    localStorage.setItem("theme", "dark");
+  } else {
+    iconoToggler.classList.add("bi-brightness-high-fill");
+    iconoToggler.classList.remove("bi-moon-stars-fill");
+    html.setAttribute("data-bs-theme", "light");
+
+    body.classList.remove("dark-mode");
+
+    localStorage.setItem("theme", "light");
+  }
+}
+
+//funcion que obtiene el tema del local storage guardado antes y setea el tema de la pagina.
+function temaLocalStorage() {
+  let tema = localStorage.getItem("theme");
+  switch (tema) {
+    case "dark":
+      iconoToggler.classList.remove("bi-brightness-high-fill");
+      iconoToggler.classList.add("bi-moon-stars-fill");
+      html.setAttribute("data-bs-theme", "dark");
+      body.classList.add("dark-mode");
+      break;
+    case (null, "light"):
+      iconoToggler.classList.add("bi-brightness-high-fill");
+      iconoToggler.classList.remove("bi-moon-stars-fill");
+      html.setAttribute("data-bs-theme", "light");
+      body.classList.remove("dark-mode");
+      break;
+  }
+}
+window.onload = temaLocalStorage;
+
+//evento click enviado al icono.
+let body = document.querySelector("body");
+let html = document.getElementsByTagName("html")[0];
+let iconoToggler = document.getElementById("iconoToggler");
+iconoToggler.addEventListener("click", cambiarIconoSegunTema);
+
+export { cambiarIconoSegunTema, temaLocalStorage };
